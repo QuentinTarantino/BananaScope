5c83d80685556d90c47e3d1025a22f79fdc94e5a
QuentinTarantino <92651357+QuentinTarantino@users.noreply.github.com>
2025-07-10 04:54:33 +0100

    Add files via upload

---
+++ steamres.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# the actual options 
+CHOICE=$(printf "4k\n1440p\n1080p\n720p" | rofi -dmenu -p "Choose resolution:")
+
+# maps choices to stuff
+case "$CHOICE" in
+  "4k")
+    TEXT="MANGOHUD=1 gamescope -w 3840 -W 3840 -h 2160 -H 2160 --force-grab-cursor -- %command%"
+    ;;
+  "1440p")
+    TEXT="MANGOHUD=1 gamescope -w 3440 -W 3440 -h 1440 -H 1440 --force-grab-cursor -- %command%"
+    ;;
+  "1080p")
+    TEXT="MANGOHUD=1 gamescope -w 1920 -W 1920 -h 1080 -H 1080 --force-grab-cursor -- %command%"
+    ;;
+  "720p")
+    TEXT="MANGOHUD=1 gamescope -w 1280 -W 1280 -h 720 -H 720 --force-grab-cursor -- %command%"
+    ;;
+  *)
+    exit 1  #close if they do something wrong or close it
+    ;;
+esac
+
+# copies the actual thing
+echo -n "$TEXT" | wl-copy 
+
