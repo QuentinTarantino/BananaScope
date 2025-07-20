7672fb770d6d46c3af8446d20d1e6bcb2a41f89c
Tarantino <92651357+QuentinTarantino@users.noreply.github.com>
2025-07-20 04:42:44 +0100

    Update steamres.sh
    added 4k and 1080p 21:9

--- steamres.sh
+++ steamres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dmenu options
-CHOICE=$(printf "4k HDR\n4k\n1440p HDR\n1440p Widescreen\n1440p\n1080p\n720p\n8k (Not Recommended)" | rofi -dmenu -p "Choose resolution:")
+CHOICE=$(printf "4k HDR\n4k Widescreen\n4k\n1440p HDR\n1440p Widescreen\n1440p\n1080p Widescreen\n1080p\n720p\n8k (Not Recommended)" | rofi -dmenu -p "Choose resolution:")
 
 KEY_DELAY="0"
 KEY_HOLD="0"
@@ -11,6 +11,9 @@ HDR_NITS="1300"
 case "$CHOICE" in
   "4k HDR")
     TEXT="MANGOHUD=1 gamescope -w 3840 -W 3840 -h 2160 -H 2160 --force-grab-cursor --hdr-enabled --hdr-sdr-content-nits $HDR_NITS --hdr-debug-force-output -- %command%"
+    ;;
+  "4k Widescreen")
+    TEXT="MANGOHUD=1 gamescope -w 5160 -W 5160 -h 2160 -H 2160 --force-grab-cursor --force-grab-cursor -- %command%"
     ;;
   "4k")
     TEXT="MANGOHUD=1 gamescope -w 3840 -W 3840 -h 2160 -H 2160 --force-grab-cursor -- %command%"
@@ -23,6 +26,9 @@ case "$CHOICE" in
     ;;
   "1440p")
     TEXT="MANGOHUD=1 gamescope -w 2560 -W 2560 -h 1440 -H 1440 --force-grab-cursor -- %command%"
+    ;;
+  "1080p Widescreen")
+    TEXT="MANGOHUD=1 gamescope -w 2560 -W 2560 -h 1080 -H 1080 --force-grab-cursor -- %command%"
     ;;
   "1080p")
     TEXT="MANGOHUD=1 gamescope -w 1920 -W 1920 -h 1080 -H 1080 --force-grab-cursor -- %command%"
